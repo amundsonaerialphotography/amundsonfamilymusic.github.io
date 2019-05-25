cc8a94b0cd302ff589090d555974dc8aef9b841a
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2019-05-25 14:29:31 -0600

    Update script.js

--- Teannes_emoji/Start/script.js
+++ Teannes_emoji/Start/script.js
@@ -7,3 +7,13 @@ open("safari://http://amundsonfamilymusic.com/Teannes_emoji/Start/");
 //window.open("http://amundsonfamilymusic.com/Teannes_emoji/"); 
 }
 
+function test() {
+
+  alert('The one I was trying to get to work on snapchat');
+open("fb://profile/318802922093552");
+  
+//window.open("http://amundsonfamilymusic.com/Teannes_emoji/"); 
+}
+
+
+
