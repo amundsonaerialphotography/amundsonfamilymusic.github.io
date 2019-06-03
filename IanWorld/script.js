9c037043e9bbe8feec31f49a76aca5af8950681f
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2019-06-03 15:27:01 -0600

    Update script.js

--- IanWorld/script.js
+++ IanWorld/script.js
@@ -1,4 +1,10 @@
 send() {
-  emailjs.send("gmail", "ira", );
+  var template_params = {
+}
+
+var service_id = "default_service";
+var template_id = "ira";
+emailjs.send(service_id, template_id, template_params);
+  
   alert("Thanks for downloading!");
 }
