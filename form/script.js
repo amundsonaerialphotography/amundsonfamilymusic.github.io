2b8e5ec99dd6ed83894f2ec43ce0966805482e07
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2019-06-03 18:26:39 -0600

    Update script.js

--- form/script.js
+++ form/script.js
@@ -1,1 +1,18 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+
+var name = url.searchParams.get("name");
+console.log(name);
+
+var email = url.searchParams.get("email");
+console.log(c);
+
+var subject = url.searchParams.get("subject");
+console.log(subject);
+
+var comment = url.searchParams.get("comment");
+console.log(comment);
+
+alert(`${name} ${email} ${subject} ${comment}`);
+
 
